01c9d892c3bce88698381c7f7c7945a36bc5770d
win <smykolka@gmail.com>
2024-06-22 16:08:59 +0200

    idk what to do sos

---
+++ JWT/Program.cs
@@ -0,0 +1,52 @@
+using System.Text;
+using JWT.Extensions;
+using JWT.Middlewares;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+
+// Add service with JWT authentication
+// Remember to add JWT configuration in appsettings
+builder.Services.AddAuthentication().AddJwtBearer(opt =>
+{
+    opt.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["JWT:Issuer"],
+        ValidAudience = builder.Configuration["JWT:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]!))
+    };
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Enable authorization
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Add middleware here
+// Choose one option - Extension method or UseMiddleware
+//app.ConfigureExceptionHandler();
+//app.UseMiddleware<CustomExceptionHandler>();
+
+app.Run();
